595a1478d71024d091a6c6de774023d83271055b
Miroslav Rovis <miro.rovis@croatiafidelis.hr>
2016-10-29 20:04:07 +0200

    analysis by while/for loops with text/bin output

--- tshark-hosts-conv.sh
+++ tshark-hosts-conv.sh
@@ -5,9 +5,69 @@
 # Copyright (C) 2015 Miroslav Rovis, <http://www.CroatiaFidelis.hr/>
 # Use this at your own risk!
 # released under BSD license, see LICENSE, or assume general BSD license,
-echo 
-echo "#############   Pls. read the script to use this.  ################"
-echo 
+#
+# Reset in case getopts has been used previously in the shell.
+#
+
+function show_help {
+  echo "tshark-hosts-conv.sh - analyze network traces with Tshark and Bash"
+  echo "Usage: $0 -r <PCAP file> -k <ssl.keylog_file>"
+  echo ""
+  echo -e "    -r \$PCAP_FILE is mandatory (but may not do it alone); see below"
+  echo -e "    \tfor particular uses though"
+  echo -e "    -k give the filename with the CLIENT_RANDOM... lines that belong to"
+  echo -e "    \tthe sessions in the PCAP. If those have been logged in the file"
+  echo -e "    \tdesignated by the \$SSLKEYLOGFILE environment variable (currently"
+  echo -e "    \thard-wired to value: /home/<you>/.sslkey.log) used during"
+  echo -e "    \tFirefox or some other NSS supporting browser's run, all properly set,"
+  echo -e "    \tthen you don't need to set this flag"
+}
+
+if [ $# -eq 0 ]; then
+    show_help
+    exit 0
+fi
+
+OPTIND=1	# Frankly, don't understand yet the OPTIND, nor if it is needed here.
+KEYLOGFILE=""
+
+while getopts "h?r:k:" opt;
+do
+    case "$opt" in
+    h|\?)
+		show_help
+		exit 0
+		;;
+    r)  PCAP_FILE=$OPTARG
+		echo "gives: -r $PCAP_FILE (\$PCAP_FILE); since \$OPTARG: $OPTARG"
+		#read FAKE
+        ;;
+    k)  KEYLOGFILE=$OPTARG
+    echo "gives: -k $KEYLOGFILE (\$KEYLOGFILE); since \$OPTARG: $OPTARG"
+    #read FAKE
+        ;;
+    esac
+done
+
+echo \$SSLKEYLOGFILE: $SSLKEYLOGFILE
+if [ "$KEYLOGFILE" == "" ]; then
+	KEYLOGFILE=$SSLKEYLOGFILE
+fi
+echo \$KEYLOGFILE: $KEYLOGFILE
+#read FAKE
+
+echo \$PCAP_FILE: $PCAP_FILE
+#read FAKE
+dump=$(echo $PCAP_FILE|cut -d. -f1)
+echo \$dump: $dump
+#read FAKE
+ext=$(echo $PCAP_FILE|cut -d. -f2)
+echo \$ext: $ext
+#read FAKE
+filename=$dump.$ext
+echo \$filename: $filename
+read FAKE
+echo \$ext: $ext
 
 function ask()	# this function borrowed from Advanced BASH Scripting Guide
 				# by Mandel Cooper (IIRC)
@@ -19,49 +79,108 @@ function ask()	# this function borrowed from Advanced BASH Scripting Guide
     esac
 }
 
-if [ $# -eq 0 ]; then
-    echo "Must give a PCAP file (and I won't check if it is one)."
-	echo "Use this at your own risk!"
-	echo "Pls. read some more in the script."
-    exit 0
+#raw=$1
+#dump=$(echo $raw|sed 's/\.pcap//')	#obviously, if the ext of your PCAP not
+#								# '.pcap', modify. I don't bother with old
+#								# .pcap (I came to network reading later),
+#								# rather I rename all new .pcapng to .pcap
+
+# Often it's logins and passwords that are of interest in traces, and typically
+# they live in POST'ed data
+tshark -o "ssl.keylog_file: $KEYLOGFILE" -r $dump.$ext -V -Y \
+	'http.request.method==POST'\
+	> $dump.POST && ls -l $dump.POST  &
+echo "-Y http.request.method==POST started in background..."
+
+# Examining $dump.POST in a session where you logged in somewhere, gets you the
+# frame.number's and tcp.stream's that contain the POST'ed data, and with which
+# IP the conversation with the login was.
+#
+# Not all can be done from this terminal. You need to view and analyze the
+# files that will be created in the process, such as $dump.POST, in another
+# terminal, to make clever decision about what IPs, what filters on them etc.
+#
+# E.g., if your login was once in entire session, you can go for either the
+# frame.number or the tcp.stream that contains it, and after one more section
+# (
+# the next section gets all http.full_uri's out on entire trace, and also
+# offers you to do filtering for any filter including frame.number or
+# tcp.stream on the whole trace
+#
+# NOTE: this filtering on tcp.stream==NNN is not extracting. This is, I guess,
+# dissection... To extract tcp.streams use:
+#
+# tshark-streams.sh
+# from
+# https://github.com/miroR/tshark-streams
+# )
+# , look up the conversation with that IP more closely.
+#
+
+echo "Run tshark-http-uri.sh on the whole trace?"
+ask;
+if [ "$?" == 0 ]; then
+	tshark-http-uri.sh -k $KEYLOGFILE -r $dump.${ext}
+	echo "As long as you want to run tshark with filtering like"
+	echo "tshark -r $dump.${ext} -Y \"\$the_filter\" ?"
+	echo "give value \"1\" when asked."
+	echo -n "1 for do filtering, 0 for none: "
+	read do_filtering
+	while [ "$do_filtering" == "1" ]; do
+		echo "Give filter (e.g. \"http.cookie\" or \"frame.number==NNN\")"
+		echo -n "to run on $dump.${ext} : "
+		read the_filter
+		tshark -o "ssl.keylog_file: $KEYLOGFILE" -r \
+			$dump.${ext} -V -Y $the_filter
+		echo "You now want to save that stdout?"
+		ask;
+		if [ "$?" == 0 ]; then
+			tshark -o "ssl.keylog_file: $KEYLOGFILE" -r \
+				$dump.${ext} -V -Y $the_filter \
+			> ${dump}_${the_filter}.txt
+			echo
+			ls -l ${dump}_${the_filter}.txt
+			echo
+			echo "---"
+			echo
+		fi
+		echo -n "1 for do filtering, 0 for none: "
+		read do_filtering
+	done
 fi
 
-raw=$1
-pcap=$(echo $raw|sed 's/\.pcap//')	#obviously, if the ext of your PCAP not
-								# '.pcap', modify
-								# I don't bother with old .pcap (I came to this
-								# later), but rename all new .pcapng to .pcap
-tshark -r $pcap.pcap -qz hosts >  $pcap.hosts && echo "$pcap.hosts created" &
-tshark -r $pcap.pcap -qz conv,ip >  $pcap.conv-ip && echo "$pcap.conv-ip created" &
+tshark -o "ssl.keylog_file: $KEYLOGFILE" -r $dump.$ext -qz hosts \
+	>  $dump.hosts && ls -l $dump.hosts &
+	echo "-qz hosts started in background..."
+tshark -o "ssl.keylog_file: $KEYLOGFILE" -r $dump.$ext -qz conv,ip \
+	>  $dump.conv-ip \
+	&& ls -l $dump.conv-ip &
+	echo "-qz conv,ip started in background..."
 
 echo 
-echo "(Possibly) wait (non-auto, human choice-wait) for:"
+echo "(Possibly) wait (non-auto, human decision-wait) for these:"
 echo 
-echo "$pcap.hosts"
+echo "$dump.hosts"
+echo "$dump.conv-ip"
 echo 
-echo "and"
+echo "to be created, and then get a useable listing"
+echo "of conversations by non-local IPs"
 echo 
-echo "$pcap.conv-ip"
-echo 
-echo "to be created, and then"
-echo 
-echo "get a useable listing of conversations by non-local IPs"
-echo 
-echo \$pcap.conv-ip: $pcap.conv-ip
-# This is a fake read me. It only waits for you to hit Enter to go on, or hit
+echo \$dump.conv-ip: $dump.conv-ip
+# This is a fake read. It only waits for you to hit Enter to go on, or hit
 # Ctrl-C and bail out.
 read FAKE
-raw_lines=$(cat $pcap.conv-ip | wc -l)
+raw_lines=$(cat $dump.conv-ip | wc -l)
 echo \$raw_lines: $raw_lines
 if [ "$raw_lines" -lt "1" ]; then
 	echo
 	echo "Yes, the below is why this happens!"
 	echo
-	echo "Maybe just check that $pcap.conv-ip is created,"
-	echo "and then hit Enter ? Yes! Those run in the background..."
+	echo "Maybe just check that $dump.conv-ip is created,"
+	echo "and then hit Enter ? Yes! Those run in the background... (faster)"
 	echo
 	echo "Then, pls. just try not to hit Enter too quickly"
-	echo "(some one-three times at start only) ?"
+	echo "(just one time at start only --if some readme FAKEs not commented out) ?"
 	echo
 	echo "\$raw_lines can't be less then 1"
 	exit 1
@@ -74,9 +193,9 @@ clean_lines=$(echo $raw_lines_sans_btm-5|bc)
 echo \$clean_lines: $clean_lines
 read FAKE
 
-cat $pcap.conv-ip | head -$raw_lines_sans_btm | tail -$clean_lines \
+cat $dump.conv-ip | head -$raw_lines_sans_btm | tail -$clean_lines \
 	| awk '{ print $1 }' > con-ip_column_1
-cat $pcap.conv-ip | head -$raw_lines_sans_btm | tail -$clean_lines \
+cat $dump.conv-ip | head -$raw_lines_sans_btm | tail -$clean_lines \
 	| awk '{ print $3 }'  > con-ip_column_3
 
 # I'm not really a programmer... If you want to use this script for yourself,
@@ -93,7 +212,7 @@ echo "A huge trace? Don't reply \"y\", would go two loops not one."
 ask;
 if [ "$?" == 0 ]; then
 echo "---";
-echo "This iss a listing of non-local IPs"
+echo "This is a listing of non-local IPs"
 echo "---";
 paste con-ip_column_1 con-ip_column_3 | grep -Ev \
 	'0\.0\.0\.0|224\.0\.0\.1|255\.255\.255\.255|127\.0\.0\.1' \
@@ -103,20 +222,87 @@ fi
 
 echo "---";
 echo "Saving the listing of non-local IPs from the trace to:"
-echo "    $pcap.non-local-hosts-ls-1"
+echo "    $dump.non-local-hosts-ls-1"
 echo "(takes time for huge traces)"
 echo "---";
 paste con-ip_column_1 con-ip_column_3 | grep -Ev \
 	'0\.0\.0\.0|224\.0\.0\.1|255\.255\.255\.255|127\.0\.0\.1' \
 	| sed 's/192.168.1.1\t//' | sed 's/\t192.168.1.1//' \
-	| sed 's/192.168.1.4\t//' | sed 's/\t192.168.1.4//' > $pcap.non-local-hosts-ls-1
+	| sed 's/192.168.1.4\t//' | sed 's/\t192.168.1.4//' \
+	> $dump.non-local-hosts-ls-1
 
-echo \$pcap.pcap: $pcap.pcap
-# legend ought to be inserted every time
+echo "This will be the first run, so you get some measure on the"
+echo "conversations, and can decide which one to analyze further"
+echo "in the second run."
+echo
+echo "Second run (if you choose it) will have options on each item."
+echo
+echo "First run now..."
+echo \$dump.\$ext: $dump.$ext
 read FAKE
-for j in $(cat $pcap.non-local-hosts-ls-1); do
-	grep $j $pcap.hosts ; read FAKE;
-	cat $pcap.conv-ip | head -5 | tail -2
-	grep $j $pcap.conv-ip ;
+# legend ought to be inserted every time
+for j in $(cat $dump.non-local-hosts-ls-1); do
+	grep $j $dump.hosts ; read FAKE;
+	cat $dump.conv-ip | head -5 | tail -2
+	grep $j $dump.conv-ip ;
 	echo "---"; echo ; read FAKE;
 done ;
+echo "Do the second run?"
+ask;
+if [ "$?" == 0 ]; then
+	echo "Second run now... If you don't reply \"y\", no further"
+	echo "analysis will be performed for the particular IP."
+	# You can simply hit Enter, script will just continue.
+	read FAKE
+	for j in $(cat $dump.non-local-hosts-ls-1); do
+		grep $j $dump.hosts ; read FAKE;
+		cat $dump.conv-ip | head -5 | tail -2
+		grep $j $dump.conv-ip ;
+		echo "The IP: $j"
+		read FAKE
+		echo "Save the conversation as separate PCAP?"
+		ask;
+		if [ "$?" == 0 ]; then
+			tshark -o "ssl.keylog_file: $KEYLOGFILE" -r $dump.$ext -Y "(ip.addr==$j)" -w ${dump}_${j}.${ext}
+			echo
+			ls -l ${dump}_${j}.${ext}
+			echo
+			echo "Run tshark-http-uri.sh on:"
+			echo "${dump}_${j}.${ext}"
+			echo "(the newly created)?"
+			ask;
+			if [ "$?" == 0 ]; then
+				new_dump=${dump}_${j}
+				echo \$new_dump: $new_dump
+				tshark-http-uri.sh -k $KEYLOGFILE -r $new_dump.${ext}
+				echo "As long as you want to run tshark with filtering like"
+				echo "tshark -r $new_dump.${ext} -Y \"\$the_filter\" ?"
+				echo "give value \"1\" when asked."
+				echo -n "1 for do filtering, 0 for none: "
+				read do_filtering
+				while [ "$do_filtering" == "1" ]; do
+					echo "Give filter (e.g. \"http.cookie\" or \"frame.number==NNN\")"
+					echo -n "to run on $new_dump.${ext} : "
+					read the_filter
+					tshark -o "ssl.keylog_file: $KEYLOGFILE" -r \
+						$new_dump.${ext} -V -Y $the_filter
+					echo "You now want to save that stdout?"
+					ask;
+					if [ "$?" == 0 ]; then
+						tshark -o "ssl.keylog_file: $KEYLOGFILE" -r \
+							$new_dump.${ext} -V -Y $the_filter \
+						> ${new_dump}_${the_filter}.txt
+						echo
+						ls -l ${new_dump}_${the_filter}.txt
+						echo
+						echo "---"
+						echo
+					fi
+					echo -n "1 for do filtering, 0 for none: "
+					read do_filtering
+				done
+			fi
+		fi
+		echo "---"; echo ; read FAKE;
+	done ;
+fi
---
+++ tshark-http-uri.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+#
+# a simple script to extract full_uri's from PCAPs
+#
+#	part of the set tshark-hosts-conv, but
+#	can be used separately.
+#
+# Copyright (C) 2015 Miroslav Rovis, <http://www.CroatiaFidelis.hr/>
+# Use this at your own risk!
+# released under BSD license, see LICENSE, or assume general BSD license,
+#
+
+function show_help {
+  echo "tshark-http-uri.sh - dump full_uri's of a PCAP by frame number"
+  echo "Usage: $0 -r <PCAP file> -k <ssl.keylog_file>"
+  echo ""
+  echo -e "    -r \$PCAP_FILE is mandatory (but may not do it alone); see below"
+  echo -e "    \tfor particular uses though"
+  echo -e "    -k give the filename with the CLIENT_RANDOM... lines that belong to"
+  echo -e "    \tthe sessions in the PCAP. If those have been logged in the file"
+  echo -e "    \tdesignated by the \$SSLKEYLOGFILE environment variable (currently"
+  echo -e "    \thard-wired to value: /home/<you>/.sslkey.log) used during"
+  echo -e "    \tFirefox or some other NSS supporting browser's run, all properly set,"
+  echo -e "    \tthen you don't need to set this flag"
+}
+
+if [ $# -eq 0 ]; then
+    show_help
+    exit 0
+fi
+
+# Reset in case getopts has been used previously in the shell.
+OPTIND=1	# Frankly, don't understand yet the OPTIND, nor if it is needed here.
+KEYLOGFILE=""
+
+while getopts "h?r:k:" opt;
+do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    r)  PCAP_FILE=$OPTARG
+		echo "gives: -r $PCAP_FILE (\$PCAP_FILE); since \$OPTARG: $OPTARG"
+		#read FAKE
+        ;;
+    k)  KEYLOGFILE=$OPTARG
+		echo "gives: -k $KEYLOGFILE (\$KEYLOGFILE); since \$OPTARG: $OPTARG"
+		#read FAKE
+        ;;
+    esac
+done
+
+echo \$SSLKEYLOGFILE: $SSLKEYLOGFILE
+if [ "$KEYLOGFILE" == "" ]; then
+	KEYLOGFILE=$SSLKEYLOGFILE
+fi
+echo \$KEYLOGFILE: $KEYLOGFILE
+#read FAKE
+
+echo -n \$PCAP_FILE: $PCAP_FILE
+#read FAKE
+# Files can have a few dots, this is how I'll take the last as separator.
+num_dots=$(echo $PCAP_FILE|sed 's/\./\n/g'| wc -l)
+num_dots_min_1=$(echo $num_dots - 1 | bc)
+echo \$num_dots: $num_dots
+echo \$num_dots_min_1: $num_dots_min_1
+ext=$(echo $PCAP_FILE|cut -d. -f $num_dots)
+echo \$ext: $ext
+#read FAKE
+echo $PCAP_FILE|sed "s/\(.*\)\.$ext/\1/"
+dump=$(echo $PCAP_FILE|sed "s/\(.*\)\.$ext/\1/")
+echo \$dump: $dump
+#read FAKE
+filename=$dump.$ext
+echo \$filename: $filename
+#read FAKE
+echo \$ext: $ext
+read FAKE
+
+# If your SSLKEYLOGFIE is all set with the SSL keys for the PCAP, don't give a
+# second argument, but if maybe it is a PCAP not taken by your system, then get
+# the pertaining SSL keys and give the file containing them as second argument.
+
+#raw=$1
+#i=$(echo $raw|sed 's/\.pcap//')	#obviously, if the ext of your PCAP not
+									# '.pcap', modify
+#The below would need to be further revised, as I updated since. will probably
+# be dopped, w/o frame numbers it's not so useful...
+#This line sorts the uri's alphabetically, if you uncomment two lines
+#  1 + prolongation
+#tshark -o "ssl.keylog_file: $2" -q -r $i.pcap -T fields \
+#	-e 'http.request.full_uri' | sort -u > ${i}-http-request-full_uri.txt
+#This line only greps for lines with founds -- no alpha after numbers and
+# space, not grep'ed in. Good for looking up that frame number in Wireshark
+#if [ -e "$2" ]; then
+#read FAKE
+#echo $2
+# I'm duplicating the below as I currently don't know better.
+tshark -o "ssl.keylog_file: $KEYLOGFILE" -q -r $dump.$ext -T fields \
+	-e 'frame.number' -e 'http.request.full_uri' | grep \
+	-E '^[0-9]{1,9}[[:space:]][[:alpha:]]' \
+	> ${dump}-frame-http-request-full_uri.txt
+	ls -l ${dump}-frame-http-request-full_uri.txt
+#else
+#tshark -q -r $i.pcap -T fields \
+#	-e 'frame.number' -e 'http.request.full_uri' | grep \
+#	-E '^[0-9]{1,9}[[:space:]][[:alpha:]]' \
+#	> ${i}-frame-http-request-full_uri.txt
+#fi
